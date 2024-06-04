7e10105d2e24ed9c8850bc0021f684a6aeaa481a
KeeratiNon <keeratinon3@gmail.com>
2024-06-04 15:53:11 +0700

    create-movie-list-card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
+import movies from './data/movies'
 
 function App() {
   return (
     <div className="App">
-      <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
-      </section>
+    <h1>Movie List Section</h1>
+      {
+        movies.map((data)=>{
+          return (
+            <section className="movie-list-section">
+              <img src={data.image} alt="" width="102px" height="100px"/>
+              <div className="content">
+                <div className="title">Title: {data.title}</div>
+                <div className="year">Year: {data.year}</div>
+                <div className="runtime">Runtime: {data.runtime}s</div>
+                <div className="genres"><span>Genres: </span>{data.genres.map((name)=><span className="genres-box">{name}</span>)}</div>
+                <div className="rating">IMDB Ratings: {data.imdbRating}</div>
+                <div className="vote">IMDB Votes: {data.imdbVotes}</div>
+              </div>
+            </section>
+          )
+        })
+      }
     </div>
   );
 }
