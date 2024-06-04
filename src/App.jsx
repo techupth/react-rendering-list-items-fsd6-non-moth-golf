a4caad4d0924cd59dbc9c008624c1e9c525f84c3
fossi13 <fossi13@users.noreply.github.com>
2024-06-04 15:40:48 +0700

    fix: scr data and show

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import moives from "./data/movies";
 
 function App() {
   return (
     <div className="App">
-      <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+      <section className="movie-list-sectison">
+        {moives.map((moives, index) => {
+          return (
+            <div key={index} className="container">       
+              <img src={moives.image} className="img1"></img>
+              <main className="content" >
+                <p>Title: {moives.title}</p>
+                <p>Year:   {moives.year}</p>
+                <p>Runtime:  {moives.runtime}</p>
+                <p>Genres:   {moives.genres[0]} {moives.genres[1]} {moives.genres[2]}</p>
+                <p>IMDB Ratings:  {moives.imdbRating}</p>
+                <p>IMDB Votes:  {moives.imdbVotes}</p>
+              </main>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
